0620be14ef9b8010293c91b9ab9519eab0f21570
Paul DeLong <Paul.DeLong@gmail.com>
2014-05-04 22:56:48 -0400

    I had a few minutes, so I ported the simplest function, and gave it some tests;

--- src/xkcdpwgen/core.clj
+++ src/xkcdpwgen/core.clj
@@ -1,7 +1,20 @@
-(ns xkcdpwgen.core
-  (:gen-class))
+(ns xkcdpwgen.core (:gen-class))
+
+; For "bits", I'm emulating the behaviour of the original Python code.  I'm not
+; yet sure why he chose to do it that way, but the code the author wrote
+; returns one less than the actual number of bits needed to encode the input
+; number.  Maybe he had a good reason for this; maybe I'll adopt the other
+; convention at a later time.
+
+(defn bits [n]
+   (cond
+      (< n 2) 0
+      :else (+ 1 (bits (/ n 2)))
+   )
+)
 
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+   "I don't do a whole lot ... yet."
+   [& args]
+   (println "Hello, World!")
+)
--- test/xkcdpwgen/core_test.clj
+++ test/xkcdpwgen/core_test.clj
@@ -1,7 +1,12 @@
 (ns xkcdpwgen.core-test
-  (:require [clojure.test :refer :all]
-            [xkcdpwgen.core :refer :all]))
+   (:require
+      [clojure.test   :refer :all]
+      [xkcdpwgen.core :refer :all]
+   )
+)
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest zero  (testing "0" (is (= 0 (bits 0)))))
+(deftest one   (testing "1" (is (= 0 (bits 1)))))
+(deftest two   (testing "2" (is (= 1 (bits 2)))))
+(deftest three (testing "3" (is (= 1 (bits 3)))))
+(deftest four  (testing "4" (is (= 2 (bits 4)))))
