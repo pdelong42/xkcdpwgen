250c27508c27b13c4fa0c8e3712f394658e43ef2
Paul DeLong <pd72@nyu.edu>
2014-05-06 11:07:48 -0400

    implemented (readwords ...); wrote some comments; inserted commented-out Python for makebylen(), for reference;

--- src/xkcdpwgen/core.clj
+++ src/xkcdpwgen/core.clj
@@ -1,5 +1,5 @@
 (ns xkcdpwgen.core
-   (:require [clojure.string :refer [lower-case split trim]])
+   (:require [clojure.string :refer [lower-case split split-lines trim]])
    (:gen-class))
 
 ; For "bits", I'm emulating the behaviour of the original Python code.  I'm not
@@ -7,6 +7,12 @@
 ; returns one less than the actual number of bits needed to encode the input
 ; number.  Maybe he had a good reason for this; maybe I'll adopt the other
 ; convention at a later time.
+
+; It drives me nuts that I do division here, when I'd rather just be shifting
+; 'n' one bit to the right (much less taxing than division).  However, I don't
+; yet know how to do bit twiddling in Clojure.  There's a part of me that just
+; wants to do ln(n)/ln(2) and call it a day, but doing a logarithm is even more
+; taxing than division.
 
 (defn bits [n]
    (if
@@ -16,6 +22,23 @@
 
 (defn normalform [w]
    (lower-case (first (split (trim w) #"'"))))
+
+; Let's just hardcode the dictionary path until I can think of a better way to
+; handle it (preferably something idiomatic to Clojure).  Figuring-out a way to
+; test this is going to be interesting.
+
+(defn readwords []
+   (split-lines (slurp "/usr/share/dict/words")))
+
+;def makebylen():
+;    bylen = dict()
+;    for w in readwords():
+;        w = normalform(w)
+;        l = len(w)
+;        if not bylen.has_key(l):
+;            bylen[l] = dict()
+;        bylen[l][w] = 1
+;    return bylen
 
 (defn -main
    "I don't do a whole lot ... yet."
