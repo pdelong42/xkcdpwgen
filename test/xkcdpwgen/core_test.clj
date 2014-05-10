90b1facaa5bf09aa6ce160a47af0ae19a0faa6b9
Paul DeLong <pd72@nyu.edu>
2014-05-10 15:39:30 -0400

    renamed the tests to match the functions they're testing;

--- test/xkcdpwgen/core_test.clj
+++ test/xkcdpwgen/core_test.clj
@@ -1,20 +1,18 @@
 (ns xkcdpwgen.core-test
    (:require
       [clojure.test   :refer :all]
-      [xkcdpwgen.core :refer :all]
-   )
-)
+      [xkcdpwgen.core :refer :all]))
 
-(deftest bits-zero  (testing "0" (is (= 0 (bits 0)))))
-(deftest bits-one   (testing "1" (is (= 0 (bits 1)))))
-(deftest bits-two   (testing "2" (is (= 1 (bits 2)))))
-(deftest bits-three (testing "3" (is (= 1 (bits 3)))))
-(deftest bits-four  (testing "4" (is (= 2 (bits 4)))))
+(deftest bits-0 (testing "0" (is (= 0 (bits 0)))))
+(deftest bits-1 (testing "1" (is (= 0 (bits 1)))))
+(deftest bits-2 (testing "2" (is (= 1 (bits 2)))))
+(deftest bits-3 (testing "3" (is (= 1 (bits 3)))))
+(deftest bits-4 (testing "4" (is (= 2 (bits 4)))))
 
-(deftest normal-1 (testing "normal1" (is (= "this" (normalform "   this   ")))))
-(deftest normal-2 (testing "normal2" (is (= "here" (normalform "Here's another test")))))
-(deftest normal-3 (testing "normal3" (is (= "and another" (normalform "   AND another")))))
-(deftest normal-4 (testing "normal4" (is (= "" (normalform "'I wonder what this will do")))))
+(deftest normalform-1 (testing "normal1" (is (= "this" (normalform "   this   ")))))
+(deftest normalform-2 (testing "normal2" (is (= "here" (normalform "Here's another test")))))
+(deftest normalform-3 (testing "normal3" (is (= "and another" (normalform "   AND another")))))
+(deftest normalform-4 (testing "normal4" (is (= "" (normalform "'I wonder what this will do")))))
 
 (deftest makebylen-1 (testing "makebylen1" (is (= {3, ["foo" "bar"], 5 ["fubar"]} (makebylen '("foo" "bar" "fubar"))))))
 (deftest makebylen-2 (testing "makebylen2" (is (= {3, ["foo" "bar"], 5 ["fubar"]} (makebylen '(foo bar fubar))))))
