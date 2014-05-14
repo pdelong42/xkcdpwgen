14bce12334c2d684c7373b787ff5204d07300833
Paul DeLong <pd72@nyu.edu>
2014-05-14 09:15:38 -0400

    I'm fine with the default target path; I moved the dependencies last;

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
 (defproject xkcdpwgen "0.1.0-SNAPSHOT"
+   :main ^:skip-aot xkcdpwgen.core
+   :profiles {:uberjar {:aot :all}}
    :dependencies
    [  [org.clojure/algo.generic "0.1.2"]
       [org.clojure/clojure      "1.5.1"]
-      [org.clojure/tools.cli    "0.3.1"]  ]
-   :main ^:skip-aot xkcdpwgen.core
-   :target-path "target/%s"
-   :profiles {:uberjar {:aot :all}})
+      [org.clojure/tools.cli    "0.3.1"]  ]  )
